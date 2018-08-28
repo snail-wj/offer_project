cac1b42a88bf48bf8ba3afee87e79584bc2f3857
wj <2927592708@qq.com>
2018-08-28 08:03:09 +0800

    冒泡排序添加注释

--- src/main/java/com/sort/BubbleSort.java
+++ src/main/java/com/sort/BubbleSort.java
@@ -14,7 +14,9 @@ public class BubbleSort {
         System.out.println("排序前数组为:");
         Arrays.stream(arr).forEach(i -> System.out.print(i + "\t"));
         System.out.println();
+        //外层循环控制排序趟数
         for (int i = 0; i < arr.length - 1; i++) {
+            //内层循环控制每一趟排序多少次
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
